03bc43d8a5caae29110b36143a74135684578172
Virgílio Esteves <Virglio Esteves>
2016-01-09 19:08:21 +0000

    Initial version

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+# Set the base image to Node (slim version)
+FROM node:slim
+
+# Install nodemon
+RUN npm install -g nodemon
+
+# Work directory
+RUN mkdir -p /src
+WORKDIR /src
+ADD . /src
+RUN npm install
+
+# Expose HTTP server
+EXPOSE 8080
+
+# Run app
+CMD ["nodemon", "/src/app.js"]
---
+++ app.js
@@ -0,0 +1,14 @@
+/// <reference path="typings/tsd.d.ts" />
+var express = require('express');
+var http = require('http');
+
+var app = express();
+
+app.get('/', function (request, response, next) {
+	response.send('Hello World!');
+});
+
+var httpServerPort = process.env.PORT || 8080;
+http.createServer(app).listen(httpServerPort, function () {
+	console.log('Listening on port ' + httpServerPort);
+});
